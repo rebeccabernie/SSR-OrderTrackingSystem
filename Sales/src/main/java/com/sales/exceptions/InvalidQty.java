e876b702be65e3c929afe5588b893193ffc1dd47
Rebecca <rebecca.kane@outlook.com>
2017-04-29 22:33:01 +0100

    Add/list orders working, trying to get exceptions working

---
+++ Sales/src/main/java/com/sales/exceptions/InvalidQty.java
@@ -0,0 +1,10 @@
+package com.sales.exceptions;
+
+@SuppressWarnings("serial")
+public class InvalidQty extends Exception{
+
+	public InvalidQty(String message) {
+        super(message);
+    }
+
+}
